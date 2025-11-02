0547245aadacd98526d89c24319d6bbe16326577
Ansh <eye.am.ansh@gmail.com>
2025-11-02 23:40:59 +0000

    Add sky130 setup scripts

---
+++ scripts/init-sky130.sh
@@ -0,0 +1,16 @@
+# channel settings so openroad/klayout install properly
+conda config --set channel_priority true
+conda config --add channels defaults
+
+# download all files for Sky130A PDK
+conda create -c litex-hub --prefix ~/.conda-sky130 open_pdks.sky130a=1.0.457_0_g32e8f23
+# clone the SRAM22 Sky130 SRAM macros
+git clone https://github.com/AnshKetchum/sram22_sky130_macros ~/sram22_sky130_macros
+
+# install all VLSI tools
+conda create -c litex-hub --prefix ~/.conda-yosys yosys=0.27_4_gb58664d44
+conda create -c litex-hub --prefix ~/.conda-openroad openroad=2.0_7070_g0264023b6
+conda create -c litex-hub --prefix ~/.conda-klayout klayout=0.28.5_98_g87e2def28
+conda create -c litex-hub --prefix ~/.conda-signoff magic=8.3.376_0_g5e5879c netgen=1.5.250_0_g178b172
+
+conda config --set channel_priority strict
